48520c6bf3afed5a3d1ddbb56357e943efbc8b94
Sujay Mahadik <mahadik203@gmail.com>
2018-07-01 23:13:19 +0530

    v1 commit

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+
+	public float Xforce;
+	public float Yforce;
+	// Use this for initialization
+	void Start () {
+		float randomNumber = Random.Range (0, 2);
+		if (randomNumber < 0.5) {
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2(Xforce, Yforce));
+		} else {
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (-Xforce, -Yforce));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D col){
+		//Rigidbody2D newVelocity = GetComponent<Rigidbody2D> ("player1");
+		//Debug.Log ("newVelocity");
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+	Rigidbody2D player;
+	public KeyCode up;
+	public KeyCode down;
+	float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (up)) {
+			player.velocity = new Vector2 (0f, speed);
+		} else if (Input.GetKey (down)) {
+			player.velocity = new Vector2 (0f, -speed);
+		} else {
+			player.velocity = new Vector2 (0f, 0f);
+		}
+	}
+}
